798a39a281e34eb54288afc217b2d052c1dddecf
Romanych <yurytimon21@gmail.com>
2020-12-16 22:34:01 +0300

    Added Apple Cheese Consumable

---
+++ src/bsu/rfe/java/group6/lab1/Timonovich/varA7/Cheese.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group6.lab1.Timonovich.varA7;
+
+public class Cheese extends Food {
+    public Cheese() {
+        super("Cheese");
+    }
+    public void consume() {
+        System.out.println(this + " is eaten");
+    }
+
+
+}
---
+++ src/bsu/rfe/java/group6/lab1/Timonovich/varA7/Consumable.java
@@ -0,0 +1,8 @@
+package bsu.rfe.java.group6.lab1.Timonovich.varA7;
+
+public interface Consumable {
+    public abstract void consume() ;
+
+
+}
+
